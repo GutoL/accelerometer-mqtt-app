2c11ed66bb631e58e79043e48f5c876af192549b
João Neto <j.alexandreeneto@gmail.com>
2019-12-12 10:01:36 -0300

    Fix when infrared data not changing

--- mqtt/detectFallThreshold.py
+++ mqtt/detectFallThreshold.py
@@ -14,10 +14,14 @@ last_infrared_value = -1
 infrared_positive_count = 0
 fall_detected_acc = False
 fall_detected_ir = False
+alertMessage = ""
 
 def is_fall_detected_on_accelerometer():
+	global last_accelerometer_value
 	if (not last_accelerometer_value):
 		return False
+
+	global alertMessage
 
 	isFall = False
 	for line in last_accelerometer_value['data']:
@@ -32,12 +36,23 @@ def is_fall_detected_on_accelerometer():
 		norm = math.sqrt( math.pow(x,2) + math.pow(y,2) + math.pow(z,2))
 
 		if norm > 20:
+			hour = datetime.datetime.fromtimestamp(timestamp/1000.0)
+			alertMessage = "Latitude:",latitude,"longitude:",longitude,"Time:",hour.strftime("%d/%b/%Y %H:%M:%S")
 			isFall = True
 
+	last_accelerometer_value = None # cleaning the las message from accelerometer
 	return isFall
 
 def is_fall_detected_on_infra_red(limit=10):
-	return infrared_positive_count >= limit
+	global infrared_positive_count
+
+	if infrared_positive_count >= limit:
+		infrared_positive_count = 0
+		return True
+	else:
+		return False
+
+	#return infrared_positive_count >= limit
 
 def on_accelerometer_message(client, userdata, message):
 	if fall_detected_acc == True:
@@ -47,37 +62,57 @@ def on_accelerometer_message(client, userdata, message):
 	global last_accelerometer_value
 	last_accelerometer_value = json.loads(message.payload)
 
+
+count_false_positive_acc = 0
 def on_infrared_message(client, userdata, message):
-	if fall_detected_acc == False or fall_detected_ir == True:
+	global fall_detected_acc
+	global count_false_positive_acc
+
+	if fall_detected_acc == False:
 		return
+
+	# if fall_detected_acc == False or fall_detected_ir == True:
+	# 	return
 
 	global last_infrared_value
 	global infrared_positive_count
 	experiment = json.loads(message.payload)
 	last_infrared_value = experiment['value']
 	print('Infrared sensor data received: ', last_infrared_value)
+
 	if last_infrared_value == True:
 		infrared_positive_count += 1
-	else:
-		infrared_positive_count = 0
+
+	elif fall_detected_acc == True and last_infrared_value == False:
+		count_false_positive_acc += 1
+		if(count_false_positive_acc >= 6):
+			fall_detected_acc = False
+			count_false_positive_acc = 0
+			infrared_positive_count = 0
+			print("False positive from accelerometer...")
+
 
 def handle_data(config):
 	tmp1 = 0
 	tmp2 = False
 	global fall_detected_acc
 	global fall_detected_ir
+	global alertMessage
 
 	while True:
-		if (tmp1 != last_accelerometer_value and is_fall_detected_on_accelerometer()):
+		if (is_fall_detected_on_accelerometer() and fall_detected_acc == False):
 			print('Fall detected by the accelerometer sensor')
 			fall_detected_acc = True
-			tmp1 = last_accelerometer_value
 
-		fall_detected_ir = is_fall_detected_on_infra_red(config["occurrences"])
-		if ((fall_detected_acc == True and fall_detected_ir == True) and tmp2 == False):
-			print("Fall detected by the infra red sensor")
-			print("Sending alert to external services...")
-			tmp2 = True
+		if (fall_detected_acc):
+			fall_detected_ir = is_fall_detected_on_infra_red(config["occurrences"])
+
+			if(fall_detected_ir):
+				print("Fall detected by the infra red sensor")
+				print("Sending alert to external services... Information about fall:",alertMessage)
+				fall_detected_acc = False
+				fall_detected_ir = False
+
 
 def setup_subscriptions(config):
 	client = mqtt.Client()
